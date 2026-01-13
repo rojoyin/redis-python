af2096fe2800a6f930406233e117d92d5612c37f
rojoyin <rojoyin@gmail.com>
2026-01-13 18:46:16 -0500

    Handle ECHO command

--- app/main.py
+++ app/main.py
@@ -13,8 +13,34 @@ def handle_connection(connection: socket):
             if not data:
                 break
 
+            semi_parsed = data.split(b"\r\n", maxsplit=2)
+            command_size_spec = semi_parsed.pop(0)
+            parsed_command_size = int(command_size_spec[1:])
+            reminder = semi_parsed.pop()
+            tokenized_reminder = reminder.split(b"\r\n", maxsplit=1)
+            main_command = tokenized_reminder.pop(0)
+            parsed_command = [main_command]
+            raw_arguments = tokenized_reminder.pop(0)
+
+            while len(parsed_command)<parsed_command_size:
+                semi_parsed_arg = raw_arguments.split(b"\r\n", maxsplit=1)
+                arg_size = int(semi_parsed_arg.pop(0)[1:])
+                arg_value = semi_parsed_arg[0][:arg_size]
+                parsed_command.append(arg_value)
+                raw_arguments = semi_parsed_arg[0][arg_size+2:]
+
+            command = parsed_command[0]
+            if command == b"PING":
+                connection.sendall(b"+PONG\r\n")
+            elif command == b"ECHO":
+                response = f"${len(parsed_command[1])}\r\n{parsed_command[1].decode('utf-8')}\r\n"
+                connection.sendall(response.encode())
+            elif command == b"COMMAND":
+                connection.sendall(b"*0\r\n")
+            else:
+                return
+
             print(f"Replying to remote: {remote_name}")
-            connection.sendall(b"+PONG\r\n")
     except Exception:
         raise
     finally:
