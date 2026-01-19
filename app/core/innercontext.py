dd353d49b95ca13014cece66974b65175e8eab10
rojoyin <rojoyin@gmail.com>
2026-01-19 17:13:09 -0500

    Add context to pass to commands

---
+++ app/core/innercontext.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from socket import socket
+
+from app.core.datastorage import DataStorage
+
+
+@dataclass
+class InnerContext:
+    store: DataStorage
+    connection: socket
