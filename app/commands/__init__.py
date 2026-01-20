265099fa1be84a31b162ec3baf10d75403a0b5b7
rojoyin <rojoyin@gmail.com>
2026-01-20 17:05:05 -0500

    Implement ECHO command

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,3 +1,3 @@
 def load_commands() -> None:
-    from app.commands.connection import ping
+    from app.commands.connection import ping, echo
     from app.commands.server import command
--- app/commands/connection/echo.py
+++ app/commands/connection/echo.py
@@ -3,11 +3,12 @@ from app.commands.registry import registry
 from app.core.innercontext import InnerContext
 
 
-@registry.register(b"PING")
+@registry.register(b"ECHO")
 class Handler(CommandHandler):
     def parse(self, args: list[bytes]) -> object:
-        return None
+        return args[1:]
 
     def execute(self, parsed: object, context: InnerContext) -> object:
-        context.connection.sendall(b"+PONG\r\n")
+        response = f"${len(parsed[0])}\r\n{parsed[0].decode('utf-8')}\r\n"
+        context.connection.sendall(response.encode())
         return None
