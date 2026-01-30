b8afe2597015eb6828d80dd095244de9a30a9774
rojoyin <rojoyin@gmail.com>
2026-01-30 13:09:01 -0500

    Add class to store parsed data

--- app/commands/connection/echo.py
+++ app/commands/connection/echo.py
@@ -1,13 +1,19 @@
+from dataclasses import dataclass
+
 from app.commands.contract import CommandHandler
 from app.commands.registry import registry
 from app.core.innercontext import InnerContext
 from app.protocol.types import BulkString, RespValue
 
 
+@dataclass
+class EchoMessage:
+    received: bytes
+
 @registry.register(b"ECHO")
 class Handler(CommandHandler):
-    def parse(self, args: list[bytes]) -> object:
-        return args
+    def parse(self, args: list[bytes]) -> EchoMessage:
+        return EchoMessage(args[0])
 
-    def execute(self, parsed: object, context: InnerContext) -> RespValue:
-        return BulkString(parsed[0])
+    def execute(self, parsed: EchoMessage, context: InnerContext) -> RespValue:
+        return BulkString(parsed.received)
