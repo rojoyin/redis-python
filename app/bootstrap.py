edd9ba040e54bc6d2c95f93aeefcaf04f28801ee
rojoyin <rojoyin@gmail.com>
2026-01-30 10:43:21 -0500

    Handle server configuration values in different script

---
+++ app/bootstrap.py
@@ -0,0 +1,16 @@
+import argparse
+
+from app.core.configstorage import ConfigStorage
+
+
+def parse_server_args(argv: list[str] | None  = None) -> argparse.Namespace:
+    server_arg_parser = argparse.ArgumentParser()
+    server_arg_parser.add_argument("--dir", type=str)
+    server_arg_parser.add_argument("--dbfilename", type=str)
+    return server_arg_parser.parse_args(argv)
+
+def configure_server(config_storage: ConfigStorage, args: argparse.Namespace) -> None:
+    if args.dir is not None:
+        config_storage.set(b"dir", args.dir.encode("utf-8"))
+    if args.dbfilename is not None:
+        config_storage.set(b"dbfilename", args.dbfilename.encode("utf-8"))
