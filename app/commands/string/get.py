99326356ba8bd08ae1434133c864c297a8b81acd
rojoyin <rojoyin@gmail.com>
2026-01-30 13:12:27 -0500

    Add class to store parsed data GET command

--- app/commands/string/get.py
+++ app/commands/string/get.py
@@ -1,16 +1,22 @@
+from dataclasses import dataclass
+
 from app.commands.contract import CommandHandler
 from app.commands.registry import registry
 from app.core.innercontext import InnerContext
 from app.protocol.types import NullBulkString, BulkString
 
 
+@dataclass
+class GetParameter:
+    parameter_name: bytes
+
 @registry.register(b"GET")
 class Handler(CommandHandler):
-    def parse(self, args: list[bytes]) -> object:
-        return args[0]
+    def parse(self, args: list[bytes]) -> GetParameter:
+        return GetParameter(args[0])
 
-    def execute(self, parsed: bytes, context: InnerContext) -> object:
-        value = context.store.get(parsed)
+    def execute(self, parsed: GetParameter, context: InnerContext) -> object:
+        value = context.store.get(parsed.parameter_name)
 
         if value is None:
             return NullBulkString()
