540354f58f027397c77b73ab35d19970725e657a
rojoyin <rojoyin@gmail.com>
2026-02-02 07:29:59 -0500

    Handle missing syntax error in config command

--- app/commands/server/config.py
+++ app/commands/server/config.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from app.commands.registry import registry
 from app.commands.contract import CommandHandler
 from app.core.innercontext import InnerContext
-from app.protocol.types import BulkString, Array
+from app.protocol.types import BulkString, Array, RespValue, Error, SimpleString
 
 
 @dataclass
@@ -13,13 +13,22 @@ class ConfigPayload:
 
 @registry.register(B"CONFIG")
 class Handler(CommandHandler):
-    def parse(self, args: list[bytes]) -> ConfigPayload:
+    def parse(self, args: list[bytes]) -> ConfigPayload | Error:
+        if not args or len(args) < 2:
+            return Error("Command syntax error")
+
         return ConfigPayload(subcommand=args[0],parameter_name=args[1])
 
-    def execute(self, parsed: ConfigPayload, context: InnerContext) -> object:
+    def execute(self, parsed: ConfigPayload, context: InnerContext) -> RespValue:
 
         if parsed.subcommand == B"GET":
             config_parameter = context.config_store.get(parsed.parameter_name)
+
+            if not config_parameter:
+                result_string = ""
+                result = [SimpleString(result_string)]
+                return Array(result)
+
             result = Array(
                 [
                     BulkString(parsed.parameter_name),
