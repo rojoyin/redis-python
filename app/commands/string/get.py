ce87192b526b0dea9d106f2c7e2d6fccd7ca50db
rojoyin <rojoyin@gmail.com>
2026-02-02 07:21:10 -0500

    Handle incomplete get commands

--- app/commands/string/get.py
+++ app/commands/string/get.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from app.commands.contract import CommandHandler
 from app.commands.registry import registry
 from app.core.innercontext import InnerContext
-from app.protocol.types import NullBulkString, BulkString
+from app.protocol.types import NullBulkString, BulkString, RespValue, Error
 
 
 @dataclass
@@ -12,10 +12,14 @@ class GetParameter:
 
 @registry.register(b"GET")
 class Handler(CommandHandler):
-    def parse(self, args: list[bytes]) -> GetParameter:
+    def parse(self, args: list[bytes]) -> GetParameter | Error:
+
+        if not args:
+            return Error("Missing key name")
+
         return GetParameter(args[0])
 
-    def execute(self, parsed: GetParameter, context: InnerContext) -> object:
+    def execute(self, parsed: GetParameter, context: InnerContext) -> RespValue:
         value = context.store.get(parsed.parameter_name)
 
         if value is None:
