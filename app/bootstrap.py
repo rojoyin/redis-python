f1c6fb23905876b82a15943fb1128fe0d50c4b10
rojoyin <rojoyin@gmail.com>
2026-02-02 10:56:22 -0500

    Logging bootstrap configuration

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -11,6 +11,8 @@ def parse_server_args(argv: list[str] | None  = None) -> argparse.Namespace:
 
 def configure_server(config_storage: ConfigStorage, args: argparse.Namespace) -> None:
     if args.dir is not None:
+        print(f"Set {args.dir=} configuration")
         config_storage.set(b"dir", args.dir.encode("utf-8"))
     if args.dbfilename is not None:
+        print(f"Set {args.dbfilename=} configuration")
         config_storage.set(b"dbfilename", args.dbfilename.encode("utf-8"))
