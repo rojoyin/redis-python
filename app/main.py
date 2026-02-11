2cc0b9095d9a98aa963f2cdd2d663a87c0a35c10
rojoyin <rojoyin@gmail.com>
2026-02-11 15:44:53 -0500

    Call function to load data from rdb file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from app.bootstrap import parse_server_args, configure_server
+from app.bootstrap import parse_server_args, configure_server, load_rdb_data
 from app.commands import load_commands
 from app.core.configstorage import ConfigStorage
 from app.core.datastorage import DataStorage
+from app.core.rdb_parser import RDBParser
 from app.server import run
 
 
@@ -11,12 +12,13 @@ def main():
 
     store = DataStorage()
     config_store = ConfigStorage()
-
+    rdb_parser = RDBParser()
     args = parse_server_args()
+
     configure_server(config_store, args)
+    load_rdb_data(store, config_store, rdb_parser)
 
     run(host="localhost", port=6379, store=store, config_store=config_store)
-
 
 
 if __name__ == "__main__":
