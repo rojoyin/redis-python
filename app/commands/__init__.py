102bd49cb1bc4f09f9116f86aca363a5941fc06f
rojoyin <rojoyin@gmail.com>
2026-02-11 15:43:31 -0500

    Export keys command to load it

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -2,4 +2,4 @@ def load_commands() -> None:
     from app.commands.connection import ping, echo
     from app.commands.server import command, config
     from app.commands.string import set, get
-
+    from app.commands.generic import keys
