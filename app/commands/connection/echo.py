fac951ce84d757fd80940bb193f33820f0f9ff40
rojoyin <rojoyin@gmail.com>
2026-02-16 13:19:50 -0500

    Handle missing argument in echo handler

--- app/commands/connection/echo.py
+++ app/commands/connection/echo.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from app.commands.contract import CommandHandler
 from app.commands.registry import registry
 from app.core.innercontext import InnerContext
-from app.protocol.types import BulkString, RespValue
+from app.protocol.types import BulkString, RespValue, Error
 
 
 @dataclass
@@ -12,7 +12,11 @@ class EchoMessage:
 
 @registry.register(b"ECHO")
 class Handler(CommandHandler):
-    def parse(self, args: list[bytes]) -> EchoMessage:
+    def parse(self, args: list[bytes]) -> EchoMessage | Error:
+
+        if not args:
+            return Error("No message provided")
+
         return EchoMessage(args[0])
 
     def execute(self, parsed: EchoMessage, context: InnerContext) -> RespValue:
