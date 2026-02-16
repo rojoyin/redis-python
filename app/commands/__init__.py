632e0fbf10cafb016c1f786ec50764e1a6b85e4a
rojoyin <rojoyin@gmail.com>
2026-02-16 15:01:57 -0500

    Export info command handler

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,5 +1,5 @@
 def load_commands() -> None:
     from app.commands.connection import ping, echo
-    from app.commands.server import command, config
+    from app.commands.server import command, config, info
     from app.commands.string import set, get
     from app.commands.generic import keys
