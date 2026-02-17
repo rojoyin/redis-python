03bb145fb1bde7c031c56e0de20c2b49105de83b
rojoyin <rojoyin@gmail.com>
2026-02-17 07:28:54 -0500

    Support replica role in local server

--- app/commands/server/info.py
+++ app/commands/server/info.py
@@ -18,13 +18,21 @@ class Handler(CommandHandler):
         return InfoPayload(subcommand=args[0]) if args else InfoPayload()
 
     def execute(self, parsed: InfoPayload, context: InnerContext) -> RespValue:
-        configs = []
+        node_role = b"role:master"
 
-        replication_configs = [b"role:master"]
+        main_node_data = context.config_store.get(b"replicaof")
+        if main_node_data:
+            node_role = b"role:slave"
+            main_node_hostname, main_node_port = main_node_data.split(b' ')
+            print(f"Main node name: {main_node_hostname}, port: {main_node_port}")
 
-        configs.append(b"\n".join(replication_configs))
+        server_info = []
+        
+        server_replication_info = [node_role]
+
+        server_info.append(b"\n".join(server_replication_info))
 
         if parsed.subcommand == b"replication":
-            return BulkString(b"\n".join(replication_configs))
+            return BulkString(b"\n".join(server_replication_info))
 
-        return BulkString(b"\n".join(configs))
+        return BulkString(b"\n".join(server_info))
