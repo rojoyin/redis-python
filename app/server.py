92396b1d11b61523b554e26d0612eacd1032ff34
rojoyin <rojoyin@gmail.com>
2026-02-18 09:30:19 -0500

    Initialize connection to main server

--- app/server.py
+++ app/server.py
@@ -8,11 +8,21 @@ from app.core.innercontext import InnerContext
 from app.protocol import resp
 
 
-def handle_connection(connection: socket.socket, store: DataStorage, config_store: ConfigStorage):
+def handle_connection(
+    connection: socket.socket,
+    store: DataStorage,
+    config_store: ConfigStorage,
+    connection_to_main_server: socket.socket | None,
+):
     try:
         remote_name = connection.getpeername()
         print(f"New connection created, remote: {remote_name}")
-        context = InnerContext(connection=connection, store=store, config_store=config_store)
+        context = InnerContext(
+            connection=connection,
+            store=store,
+            config_store=config_store,
+            connection_to_main_server=connection_to_main_server
+        )
 
         while True:
             command, args = resp.read_command(context.connection)
@@ -31,8 +41,23 @@ def run(*, store: DataStorage, config_store: ConfigStorage) -> None:
     host = config_store.get(b"host")
     port = config_store.get(b"port")
     server_socket = socket.create_server((host, port), reuse_port=True)
+    main_server_data = config_store.get(b"replicaof")
+    connection_to_main_server = None
+
+    if main_server_data:
+        main_server_address, main_server_port = main_server_data.split(b" ")
+        connection_to_main_server = socket.create_connection(
+            (
+                main_server_address.decode("utf-8"),
+                int(main_server_port.decode("utf-8"))
+            )
+        )
+
+        ping_command = "*1\r\n$4\r\nPING\r\n"
+        connection_to_main_server.sendall(ping_command.encode())
+
 
     with ThreadPoolExecutor() as executor:
         while True:
-            connection, _ = server_socket.accept()
-            executor.submit(handle_connection, connection, store, config_store)
+            connection_to_client, _ = server_socket.accept()
+            executor.submit(handle_connection, connection_to_client, store, config_store, connection_to_main_server)
